78a9841323be45cd7ecd1f04ba79b7d9bd1bac25
Your Name <71862201+pai-sr@users.noreply.github.com>
2024-12-20 16:32:08 +0900

    fix: add project path

--- src/tools/utils.py
+++ src/tools/utils.py
@@ -1,8 +1,11 @@
 import pickle
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parent.parent
 
 def make_mapping(dict_1_path: str, dict_2_path: str) -> dict:
-    with open(dict_1_path, "rb") as f:
+    with open(PROJECT_DIR / dict_1_path, "rb") as f:
         label2id = pickle.load(f)
-    with open(dict_2_path, "rb") as f:
+    with open(PROJECT_DIR / dict_2_path, "rb") as f:
         id2label = pickle.load(f)
     return label2id, id2label
