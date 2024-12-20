a98e36e7ed9a412aad553fb32d822b34169b1cf7
Your Name <71862201+pai-sr@users.noreply.github.com>
2024-12-20 13:51:52 +0900

    fix: add use_span_pooling params

--- src/model.py
+++ src/model.py
@@ -3,19 +3,26 @@ import torch.nn as nn
 from transformers import AutoModel
 
 class RelationClassifier(nn.Module):
-    def __init__(self, model_name: str, num_labels: int, dropout: float = 0.1, tokenizer_len: int = None):
+    def __init__(self, model_name: str, num_labels: int, 
+                 dropout: float = 0.1, tokenizer_len: int = None,
+                 use_span_pooling: bool = False):
         super().__init__()
         self.model = AutoModel.from_pretrained(model_name)
         self.dropout = nn.Dropout(dropout)
-        # due to span pooling, hidden size is doubled : [E1_vec; E2_vec] concat -> hidden_size * 2
-        self.classifier = nn.Linear(self.model.config.hidden_size * 2, num_labels)
+        self.use_span_pooling = use_span_pooling
+
+        if self.use_span_pooling:
+            # due to span pooling, hidden size is doubled : [E1_vec; E2_vec] concat -> hidden_size * 2
+            self.classifier = nn.Linear(self.model.config.hidden_size * 2, num_labels)
+        else:
+            self.classifier = nn.Linear(self.model.config.hidden_size, num_labels)
+        
         if tokenizer_len:
             self.model.resize_token_embeddings(tokenizer_len)
 
     def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor, 
-                labels: torch.Tensor = None, e1_start_idx: torch.Tensor = None, 
-                e1_end_idx: torch.Tensor = None, e2_start_idx: torch.Tensor = None, 
-                e2_end_idx: torch.Tensor = None) -> torch.Tensor:
+                e1_start_idx: torch.Tensor = None, e1_end_idx: torch.Tensor = None, 
+                e2_start_idx: torch.Tensor = None, e2_end_idx: torch.Tensor = None) -> torch.Tensor:
         outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         hidden_states = outputs.last_hidden_state
 
@@ -54,7 +61,9 @@ class RelationClassifier(nn.Module):
             concat_vec = self.dropout(concat_vec)
             logits = self.classifier(concat_vec)
         else:
+
             cls_output = outputs.last_hidden_state[:, 0, :]
             cls_output = self.dropout(cls_output)
             logits = self.classifier(cls_output)
+        
         return logits
