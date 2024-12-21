4d77c8713afe0a2a605e0c4d256a55269aaffa42
Your Name <71862201+pai-sr@users.noreply.github.com>
2024-12-21 13:12:27 +0900

    fix: change model directory

--- train.sh
+++ train.sh
@@ -4,7 +4,8 @@ python src/train.py \
     --valid_file data/valid_data.csv \
     --label2id_path tools/dict_label_to_num.pkl \
     --id2label_path tools/dict_num_to_label.pkl \
-    --output_dir models \
+    --output_dir outputs \
+    --model_dir models \
     --num_epochs 10 \
     --save_model \
     --focal_loss \
