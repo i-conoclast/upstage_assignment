a12c1eb414caac28e0dd07f0de9cad01b3f6f802
Your Name <71862201+pai-sr@users.noreply.github.com>
2024-12-21 13:14:06 +0900

    fix: remove outputdir

--- train.sh
+++ train.sh
@@ -4,7 +4,6 @@ python src/train.py \
     --valid_file data/valid_data.csv \
     --label2id_path tools/dict_label_to_num.pkl \
     --id2label_path tools/dict_num_to_label.pkl \
-    --output_dir outputs \
     --model_dir models \
     --num_epochs 10 \
     --save_model \
